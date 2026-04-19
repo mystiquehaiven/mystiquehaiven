e901e95e3cb837ac0e85ea154df304e2fe6db6c7
Caleb Johnson <mystiquehaiven@gmail.com>
2026-04-19 00:43:28 -0700

    Changed Home Page, Added META-Data

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-black">
-      <h1 className="text-white text-2xl font-light tracking-widest uppercase">
-        Coming Soon
-      </h1>
-      <h2>
-        MYSTIQUE hAIven
-      </h2>
-      <h3>
-        The Highest Quality NSFW AI Gallery
-      </h3>
-    </div>
+<div className="flex flex-col items-center justify-center min-h-screen bg-black">
+  <h1 className="text-white text-2xl font-light tracking-widest uppercase">
+    Coming Soon
+  </h1>
+  <h2>
+    MYSTIQUE hAIven
+  </h2>
+  <h3>
+    "LOREM IPSUM"
+  </h3>
+</div>
   );
 }
