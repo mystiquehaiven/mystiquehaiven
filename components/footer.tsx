d343725d04d5a07deea240c81c5c5b575f306735
Caleb Johnson <mystiquehaiven@gmail.com>
2026-04-26 21:18:46 -0700

    Added Privacy Policy

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import "../app/styles/footer.css"
 export default function Footer() {
   return (
     <footer className="footer">
-      <a href="/terms">Terms of Service</a>
+      <a href="/terms">Terms of Service--</a>
       <a href="/privacy">Privacy Policy</a>
     </footer>
   );
