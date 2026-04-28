7012a1d4899571af37032dd1ab3e55f763630ad5
Caleb Johnson <mystiquehaiven@gmail.com>
2026-04-27 21:10:10 -0700

    UI Tweaks

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { AuthProvider } from "@/context/AuthContext";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
     >
         <body className="min-h-full flex flex-col">
           <AuthProvider>
+            <Navbar />
             {children}
           </AuthProvider>
         </body>
