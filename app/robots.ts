c87787e5afa1c366fb9508683ecdc3a61ee68ab3
Caleb Johnson <mystiquehaiven@gmail.com>
2026-07-02 18:58:30 -0700

    changed homepage

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: "*",
-        disallow: ["/videos$", "/videos/new", "/signin", "/api/"],
       },
     ],
     sitemap: "https://mystiquehaiven.com/sitemap.xml",
