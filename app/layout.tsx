02a481f1caeed409274cd84cffb65923addd78e9
Caleb Johnson <mystiquehaiven@gmail.com>
2026-07-06 23:27:39 -0700

    Optimized

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { AuthProvider } from "@/context/AuthContext";
